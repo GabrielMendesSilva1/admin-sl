5a4062040d70900bfec26aed15ee963b1cb5da01
GabrielMendesSilva1 <gabrielmendes198e@gmail.com>
2025-05-12 17:02:57 -0300

    rota para segurados

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "../pages/login";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from "../pages/dashboard";
+import Segurado from "../pages/segurados/segurado";
 
 const AppRoutes = () => {
     return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/segurados" element={<Segurado />} />
             </Routes>
         </Router>
     );
