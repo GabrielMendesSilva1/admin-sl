2a1e479e33f2e42bfcfd796d65611ed964778f2a
GabrielMendesSilva1 <gabrielmendes198e@gmail.com>
2025-05-13 18:53:27 -0300

    Dados em mock Seguradora

---
+++ src/services/SeguradoraService.jsx
@@ -0,0 +1,33 @@
+// services/seguradorasService.js
+
+export const getMockedSeguradoras = () => [
+    {
+      id: "1",
+      nome: "Seguradora Alpha",
+      endereco: "Rua das Flores, 123",
+      bairro: "Centro",
+      cidade: "São Paulo",
+      uf: "SP",
+      cep: "01000-000",
+      fone1: "(11) 1234-5678",
+      fone2: "(11) 8765-4321",
+      contato: "João da Silva",
+      cgc: "12.345.678/0001-90",
+      email: "contato@alpha.com.br"
+    },
+    {
+      id: "2",
+      nome: "Seguradora Beta",
+      endereco: "Av. Brasil, 456",
+      bairro: "Jardins",
+      cidade: "Rio de Janeiro",
+      uf: "RJ",
+      cep: "20000-000",
+      fone1: "(21) 2345-6789",
+      fone2: "(21) 9876-5432",
+      contato: "Maria Souza",
+      cgc: "98.765.432/0001-10",
+      email: "atendimento@beta.com.br"
+    }
+  ];
+  
