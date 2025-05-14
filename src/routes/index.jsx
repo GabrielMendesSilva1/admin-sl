d1085e4cf4029426a03d06731770cec246b3e15b
GabrielMendesSilva1 <gabrielmendes198e@gmail.com>
2025-05-14 16:01:54 -0300

    fix: corrige nome de pastas, organizacao

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "../pages/login";
 import Dashboard from "../pages/dashboard";
-import Segurado from "../pages/segurados";
-import Automovel from "../pages/seguros/automovel";
-import Patrimonial from "../pages/seguros/patrimonial";
-import Seguradoras from "../pages/seguradoras";
+import Segurado from "../pages/ConsultaSegurados";
+import Automovel from "../pages/ConsultaSeguros/automovel";
+import Patrimonial from "../pages/ConsultaSeguros/patrimonial";
+import Seguradoras from "../pages/ConsultaSeguradoras";
+
+import CadastroSegurado from "../pages/cadastro/Segurado";
+import CadastroSeguradora from "../pages/cadastro/Seguradora";
+import CadastroAuto from "../pages/cadastro/Seguros/Automovel";
 
 const AppRoutes = () => {
     return (
@@ -13,10 +17,17 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+
                 <Route path="/segurados" element={<Segurado />} />
                 <Route path="/automovel/:cpfCnpj" element={<Automovel />} />
                 <Route path="/patrimonial/:cpfCnpj" element={<Patrimonial />} />
-                <Route path="/seguradoras" element={<Seguradoras />}/>
+                <Route path="/seguradoras" element={<Seguradoras />}/>\
+
+                <Route path="/cadastro/Segurado" element={<CadastroSegurado />} />
+                <Route path="/cadastro/Seguradora" element={<CadastroSeguradora />} />
+                <Route path="/cadastro/Automovel" element={<CadastroAuto />} />
+                <Route path="/cadastro/Patrimonio" element={<Login />} />
+                
             </Routes>
         </Router>
     );
